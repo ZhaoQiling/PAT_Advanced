fa910e9303065cf00b587641be8cb365886f794e
ZhaoQiling <qilingzhao8@gmail.com>
2017-11-07 21:04:41 +0800

    inOrder and some tricks

--- PAT1064.cpp
+++ PAT1064.cpp
@@ -1,13 +1,31 @@
 #include <iostream>
 #include <cstdio>
-#include <cstring>
-#define MAXN 99999
+#include <vector>
+#include <algorithm>
+#define MAXN 2000
 using namespace std;
-int arr[MAXN];
 int nNode;
+int curSeq = 0;
+vector<int> seq;
+int arr[MAXN];
+void dfs(int root){
+    if(root > nNode)
+        return;
+    dfs(root << 1);
+    arr[root] = seq[curSeq++];
+    dfs(root << 1 | 1);
+}
 int main(){
-    memset(arr, -1, sizeof(arr));
     scanf("%d", &nNode);
-    int minLoc = getMinLoc();
+    seq.resize(nNode);
+    for(int i = 0; i < nNode; i++)
+        scanf("%d", &seq[i]);
+    sort(seq.begin(), seq.end());
+    dfs(1);
+
+    for(int i = 1; i <= nNode; i++){
+        if(i != 1) printf(" ");
+        printf("%d", arr[i]);
+    }
     return 0;
 }
